5003f334acb30fd042387c5ac648ca4c54a9092b
jacksnailgo <38100880+jacksnailgo@users.noreply.github.com>
2018-11-24 15:34:20 +0800

    Create CategoryServlet.java

---
+++ src/servlet/CategoryServlet.java
@@ -0,0 +1,53 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import domain.Category;
+import service.ProductService;
+
+/**
+ * Servlet implementation class CategoryList
+ */
+public class CategoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CategoryServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductService service=new ProductService();
+		List<Category> category=service.findAllCategory();
+		//将数据转成JSON格式
+		Gson gson =new Gson();
+		String json=gson.toJson(category);
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().write(json);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
