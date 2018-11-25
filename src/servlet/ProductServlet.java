3219379b8674ce2aab75924cb36175205c227fdf
jacksnailgo <38100880+jacksnailgo@users.noreply.github.com>
2018-11-25 08:59:00 +0800

    Update ProductServlet.java

--- src/servlet/ProductServlet.java
+++ src/servlet/ProductServlet.java
@@ -26,12 +26,14 @@ import domain.Product;
 import service.ProductService;
 
 /**
- * Servlet implementation class ProductServlet
+ * 该类中没有service方法，会去执行父类的service方法，此时this指代的都是ProductServlet
  */
-public class ProductServlet extends HttpServlet {
+public class ProductServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	
+	
+	
+	/*protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String methodName = request.getParameter("method");
 		if ("categoryList".equals(methodName)) {
@@ -49,7 +51,7 @@ public class ProductServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
+	}*/
 
 	// 模块中的功能是根据方法区分的
 	// 显示商品类别的功能
@@ -210,6 +212,7 @@ public class ProductServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 		// 计算小计
+		
 		double subTotal = product.getShop_price() * buyNum;
 		// 封装cartItem
 		CartItem cartItem = new CartItem();
@@ -222,20 +225,35 @@ public class ProductServlet extends HttpServlet {
 		if (cart == null) {
 			cart = new Cart();
 		}
+		//将购物项放到车中---key是pid
+		//先判断购物车中是否已将包含此购物项了 ----- 判断key是否已经存在
+		//如果购物车中已经存在该商品----将现在买的数量与原有的数量进行相加操作
+		Map<String,CartItem> cartItems=cart.getCartItems();
+		double newSubTotal=0.0;
+		if(cartItems.containsKey(pid)) {
+			CartItem items=cartItems.get(pid);
+			int oldNum=items.getBuyNum();
+			oldNum+=buyNum;
+			items.setBuyNum(oldNum);
+			cartItems.put(pid, items);
+			cart.setCartItems(cartItems);
+		}else {
+			cart.getCartItems().put(pid, cartItem);
+		}
+				
 		//计算总计金额
 		double total=cart.getTotal()+subTotal;
 		cart.setTotal(total);
 		System.out.println(cart);
 		//将购物项放到车中--key是pid
 		//我们在cart中已经生成一个map，直接将cartItem放入购物车中。
-		cart.getCartItems().put(pid, cartItem);
+		//cart.getCartItems().put(pid, cartItem);
 		session.setAttribute("cart", cart);
 		
 		//直接跳转到购物车页面
 		//使用转发，因为使用的是session，在整个web中都保存着session。而Cookie要写代码获取
-//		request.getRequestDispatcher("/cart.jsp").forward(request, response);
-		response.sendRedirect(request.getContextPath()+"cart.jsp");
-
+	//request.getRequestDispatcher("/cart.jsp").forward(request, response);
+		response.sendRedirect(request.getContextPath()+"/cart.jsp");
 	}
 
 	public void delProductToCart(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -253,5 +271,10 @@ public class ProductServlet extends HttpServlet {
 		session.setAttribute("cart", cart);
 		response.sendRedirect(request.getContextPath()+"/cart.jsp");
 		
+	}
+	public void delAllProductToCart(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		HttpSession session=request.getSession();
+		session.removeAttribute("cart");
+		response.sendRedirect(request.getContextPath()+"/cart.jsp");
 	}
 }
